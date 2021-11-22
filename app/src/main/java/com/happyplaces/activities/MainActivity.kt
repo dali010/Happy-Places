d41f60216c57d26c693db32392c63d91addedc87
Mohamed Ali Hmida <MohamedAliHmida@takiacademyteam.com>
2021-11-22 12:58:17 +0100

    adding an onClick to recyclerView element

--- app/src/main/java/com/happyplaces/activities/MainActivity.kt
+++ app/src/main/java/com/happyplaces/activities/MainActivity.kt
@@ -44,6 +44,13 @@ class MainActivity : AppCompatActivity() {
         rv_happy_places_list.setHasFixedSize(true)
         val placesAdapter = HappyPlacesAdapter(this,happyPlaceList)
         rv_happy_places_list.adapter = placesAdapter
+
+        placesAdapter.setOnClickListener(object : HappyPlacesAdapter.OnClickListener{
+            override fun onClick(position: Int, model: HappyPlaceModel) {
+                val intent = Intent (this@MainActivity , HappyPlaceDetailActivity::class.java)
+                startActivity(intent)
+            }
+        })
     }
 
     private fun getHappyPlacesListFromLocalDB(){
--- app/src/main/java/com/happyplaces/adapters/HappyPlacesAdapter.kt
+++ app/src/main/java/com/happyplaces/adapters/HappyPlacesAdapter.kt
@@ -16,6 +16,8 @@ open class HappyPlacesAdapter(
     private var list: ArrayList<HappyPlaceModel>
 ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
+    private var onClickListener: OnClickListener? = null
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
 
         return MyViewHolder(
@@ -27,6 +29,10 @@ open class HappyPlacesAdapter(
         )
     }
 
+    fun setOnClickListener(onClickListener: OnClickListener){
+        this.onClickListener = onClickListener
+    }
+
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val model = list[position]
 
@@ -34,12 +40,24 @@ open class HappyPlacesAdapter(
             holder.itemView.iv_place_image.setImageURI(Uri.parse(model.image))
             holder.itemView.tvTitle.text = model.title
             holder.itemView.tvDescription.text = model.description
+
+            holder.itemView.setOnClickListener {
+                if (onClickListener != null){
+                    onClickListener!!.onClick(position,model)
+                }
+            }
         }
     }
 
 
+
+
     override fun getItemCount(): Int {
         return list.size
+    }
+
+    interface OnClickListener{
+        fun onClick(position: Int , model: HappyPlaceModel)
     }
 
 
